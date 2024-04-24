b3f5cd1488b11221e28c1274f7a81e1353ae84f8
IvanMarte <ivanbiasfx@gmail.com>
2024-04-24 13:53:37 -0600

    Silenciadores Section

--- modal/modal-godin.js
+++ modal/modal-godin.js
@@ -104,4 +104,37 @@ iconosDownpipe.forEach(function(icono) {
 });
 
 
-// =========Silenciadores de Alto Flujo=========00
+// =========Silenciadores de Alto Flujo=========
+
+
+
+var iconosSilenciadores = document.querySelectorAll('#modal-Godin-producto-silenciador i');
+
+iconosSilenciadores.forEach(function(icono) {
+    icono.addEventListener('click', function() {
+        // Obten el id del icono
+        var idIcono = icono.id;
+
+        // Si el producto ya fue seleccionado, deseleccionarlo
+        if (sistemaDeEscape.silenciador === idIcono) {
+            sistemaDeEscape.silenciador = null;
+            icono.classList.remove('fa-check');
+            icono.classList.add('fa-plus');
+            icono.style.color = '';
+        } else {
+            // Si otro producto fue seleccionado previamente, deseleccionarlo
+            if (sistemaDeEscape.silenciador) {
+                var iconoAnterior = document.getElementById(sistemaDeEscape.silenciador);
+                iconoAnterior.classList.remove('fa-check');
+                iconoAnterior.classList.add('fa-plus');
+                iconoAnterior.style.color = '';
+            }
+
+            // Seleccionar el nuevo producto
+            sistemaDeEscape.silenciador  = idIcono;
+            icono.classList.remove('fa-plus');
+            icono.classList.add('fa-check');
+            icono.style.color = 'green';
+        }       
+    });
+});
